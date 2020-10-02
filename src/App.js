3b6a505d4bf2b166f0409df9ceebf1ea161a41b1
chanyeong <cyjo9603@naver.com>
2020-10-03 00:24:27 +0900

    [refactor] redux 초기화 부분 index로 이동
    - App.js 보다는 index.js에 redux 설정 및 초기화 코드가 있는 것이 좀 더 맞다고 판단

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
 
-import rootStore, { saga } from './module';
 import Button from './commons/Button';
 
 import './App.css';
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootStore, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(saga);
-
 const App = () => {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Button>이미지 요청</Button>
-      </div>
-    </Provider>
+    <div className="App">
+      <Button>이미지 요청</Button>
+    </div>
   );
 };
 
