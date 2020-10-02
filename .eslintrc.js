8d0a185f97790a2b7fc62af5e5625f8a92063b2a
조찬영 <cyjo9603@naver.com>
2020-10-02 15:40:52 +0900

    env: lint 및 prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: ['react-hooks'],
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+    'linebreak-style': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
+};
